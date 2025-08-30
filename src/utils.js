4da91d50dd7049ff20bb1f5ea182427be8cadd0c
Pavel <you@example.com>
2025-08-30 14:06:22 +0300

    Implemented backgroundColor module

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,12 @@
 export function random(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1))
 }
+
+export function getRandomColor() {
+  const text = '1234567890ABCDEF';
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += text[Math.floor(Math.random() * text.length)];
+  }
+  return color;
+}
